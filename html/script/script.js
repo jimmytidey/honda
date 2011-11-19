e4b7cdf7b7c9ad0959098e29359ae8b69ee38175
jimmytidey <jimmytidey@gmail.com>
2011-11-19 17:22:50 +0000

    sorting out file

--- html/script/script.js
+++ html/script/script.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 radio = {}; 
 
 radio.init = function() {
@@ -59,28 +58,3 @@ radio.advancedMarker = function (map) {
 $(document).ready(function() {
 	radio.init();
 }); 
-=======
-home = {}; 
-
-home.sort = function() {	
-	 
- 	if (self.document.location.hash){ 
-			$("#options a").attr('class', ''); 
-			home.filter_class = "."+self.document.location.hash;
-			home.filter_class = home.filter_class.replace('#', "");
-			$('#isotope_container').isotope({filter: home.filter_class});
-			$('#options [href="'+self.document.location.hash+'"]').attr('class', 'selected');
-	}
-	else {
-		$('#isotope_container').isotope({filter: '.skills'});		
-	}
-	
-}
-
-$(document).ready(function() {
-	home.sort(); 
-	//When I'm not in a hurry I'll implement this with http://isotope.metafizzy.co/demos/hash-history.html#filter=.post-transition
-	hashChangeEventListener = setInterval("home.sort()", 50);
-});
-
->>>>>>> 4a742a625fb6731ed1e7f62719de30af49f6982a
